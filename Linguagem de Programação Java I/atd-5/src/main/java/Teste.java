c92956b6d0ea02271d8381e1a7581b9fd9d505dd
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2026-04-23 16:31:36 -0300

    Atividade 5, interfaces

--- Linguagem de Programação Java I/atd-5/src/main/java/Teste.java
+++ Linguagem de Programação Java I/atd-5/src/main/java/Teste.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class Teste {
+    private static int contador = 0;
+    private static Carga[] veiculosCarga = new Carga[10];
+    private static Passeio[] veiculosPasseio = new Passeio[10];    
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
+        String placaPesquisada = "";
 
         do {
             // Exibe o menu para o usuário
@@ -29,19 +33,21 @@ public class Teste {
                     break;
 
                 case 3:
-                    //imprimirVeiculo("p");
+                    buscarVeiculo("p", null);
                     break;
                  
                 case 4:
-                    //imprimirVeiculo("c");
+                    buscarVeiculo("c", null);
                     break;
                     
                 case 5:
-                    //imprimirVeiculo("p", "placa");
+                    placaPesquisada = scanner.nextLine();
+                    buscarVeiculo("p", placaPesquisada);
                     break;   
                     
                 case 6:
-                    //imprimirVeiculo("c", "placa");
+                    placaPesquisada = scanner.nextLine();
+                    buscarVeiculo("c", placaPesquisada);
                     break;                     
 
                 case 7:
@@ -53,5 +59,35 @@ public class Teste {
                     break;
             }
         } while (opcao != 7);
+    }
+    
+    //Método para buscar um aluno pela matrícula
+    private static void buscarVeiculo(String tipoVeiculo, String placa) {
+        Veiculo[] veiculos;
+
+        if (tipoVeiculo.equalsIgnoreCase("c")) {
+            veiculos = veiculosCarga;
+        } else {
+            veiculos = veiculosPasseio;
+        } 
+        
+        boolean encontrado = false;
+        
+        if(placa != null){
+          for (int i = 0; i < contador; i++) {
+            if (veiculos[i].getPlaca().equals(placa)) {
+                System.out.println("\nVeículo encontrado:");
+                System.out.println(veiculos[i]);
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Veículo com a placa " + placa + " não encontrado.");
+        }
+            
+        }
+
     }
 }
